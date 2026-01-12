33f9a76b18ac0b28ef059bed20bd45e0524d6416
glowch69 <glowcxh@gmail.com>
2026-01-12 10:54:01 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Vitas AI yüklendi!");
+
+// Sekme değiştirme (basit)
+document.querySelectorAll('button').forEach(btn => {
+    btn.addEventListener('click', () => {
+        alert('Sistem hazır! AI özellikleri yakında eklenecek.');
+    });
+});
